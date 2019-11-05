25a7c0a6dd84006028c65f79bd2dcea36fd079b3
Gasol Wu <gasol.wu@gmail.com>
2019-11-05 14:52:22 +0800

    Add composer.mk

---
+++ Mk/composer.mk
@@ -0,0 +1,18 @@
+COMPOSER   ?= $(shell command -v composer)
+VENDOR_DIR ?= vendor
+
+ifdef CI
+	COMPOSER_INSTALL_FLAGS+=--no-interaction
+	COMPOSER_INSTALL_FLAGS+=--no-progress
+endif
+
+composer.json:
+	$(COMPOSER) init
+
+composer.lock: composer.json
+	$(COMPOSER) update $(COMPOSER_UPDATE_FLAGS)
+	@test -e "$@" || cat > "$@" <<<'{}'
+
+$(VENDOR_DIR): composer.json composer.lock
+	$(COMPOSER) validate --strict
+	$(COMPOSER) install $(COMPOSER_INSTALL_FLAGS)
