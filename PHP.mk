1df03ddf3fe811c8bc4ac9f663884d6f8186a08a
Gasol Wu <gasol.wu@gmail.com>
2019-11-05 18:07:36 +0800

    Introduce checksyntax and clean target

--- PHP.mk
+++ PHP.mk
@@ -6,6 +6,25 @@ phpunit_DEPS := composer
 
 DEPS := $(foreach use, $(USES), $($(use)_DEPS))
 USES += $(DEPS)
-UNIQUE_USES := $(shell IFS=' ' echo $(USES) | tr ' ' '\n' | sort -u)
+UNIQUE_USES := $(shell echo $(USES) | tr ' ' '\n' | sort -u)
+
+PHP_SRCDIR ?= src
+PHP_TESTDIR ?= tests
+
+find_php_files=$(shell find $1 -type f -name '*.php')
+PHP_FILES := $(call find_php_files,$(PHP_SRCDIR))
+PHP_FILES += $(call find_php_files,$(PHP_TESTDIR))
+
+build/syntax/%.php: %.php
+	@mkdir -p $(dir $@)
+	@php -l $<
+	@touch $@
+
+.PHONY: syntax
+syntax: $(PHP_FILES:%=build/syntax/%)
+
+.PHONY: clean
+clean:
+	rm -rf build
 
 include $(foreach use, $(UNIQUE_USES), $(wildcard $(USESDIR)/$(use).mk))
