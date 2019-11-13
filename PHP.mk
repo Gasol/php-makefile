d4cfa3257a8cd08c64ec8ed8c2cf5089160bff75
Gasol Wu <gasol.wu@gmail.com>
2019-11-13 16:25:06 +0800

    Introduce $(BUILD_DIR)

--- PHP.mk
+++ PHP.mk
@@ -8,6 +8,7 @@ DEPS := $(foreach use, $(USES), $($(use)_DEPS))
 USES += $(DEPS)
 UNIQUE_USES := $(shell echo $(USES) | tr ' ' '\n' | sort -u)
 
+BUILD_DIR ?= build
 PHP_SRCDIR ?= src
 PHP_TESTDIR ?= tests
 
@@ -15,16 +16,16 @@ find_php_files=$(shell find $1 -type f -name '*.php')
 PHP_FILES := $(call find_php_files,$(PHP_SRCDIR))
 PHP_FILES += $(call find_php_files,$(PHP_TESTDIR))
 
-build/syntax/%.php: %.php
+$(BUILD_DIR)/syntax/%.php: %.php
 	@mkdir -p $(dir $@)
 	@php -l $<
 	@touch $@
 
 .PHONY: syntax
-syntax: $(PHP_FILES:%=build/syntax/%) ## Check syntax by PHP built-in linter
+syntax: $(PHP_FILES:%=$(BUILD_DIR)/syntax/%) ## Check syntax by PHP built-in linter
 
 .PHONY: clean
 clean: ## Remove temporary files during development cycle
-	rm -rf build
+	rm -rf $(BUILD_DIR)
 
 include $(foreach use, $(UNIQUE_USES), $(wildcard $(USESDIR)/$(use).mk))
